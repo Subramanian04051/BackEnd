78007f0bd7310d8419ec2c0d7893264ee8b610ee
Subramanian Muthu kumar <subramanian04051@gmail.com>
2023-11-10 16:20:46 +0530

    k value obtain by summing two elements in an array

---
+++ TwoSum.js
@@ -0,0 +1,24 @@
+[11/2 11:20 AM] Subramanian M
+let a=[1,2,3,4,5,6],sum=0,total=11
+ 
+while(sum!=total)
+
+{
+
+    i=Math.ceil(Math.random()*a.length)
+
+    j=Math.ceil(Math.random()*a.length)
+
+    if(i!=j)
+
+    sum=a[i]+a[j]
+
+    //console.log(i)
+
+}
+
+console.log(i)
+
+console.log(j)
+ 
+Two sum problem
