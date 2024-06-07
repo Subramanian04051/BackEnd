d9ef8346a10ed3077ff8035633d54c5084e5dcc0
Subramanian Muthu kumar <subramanian04051@gmail.com>
2024-06-07 18:02:03 +0530

    Update max_sum_of_i*arr[i].js

--- max_sum_of_i*arr[i].js
+++ max_sum_of_i*arr[i].js
@@ -1,17 +1,19 @@
-let a=[1,5,2,10,0],rotations=a.length-1,sum=0,max_sum=0
+let a=[1,2,3,4],rotations=a.length-1,sum=0,max_sum=0
 
 for(let i=0;i<rotations;i++)
 {
-    a.push(a[0])
-    a.shift()
+    
     for(let j=0;j<a.length;j++)
     {
         sum+=j*a[j]
     }
+    //console.log(sum)
     if(sum>max_sum)
     {
         max_sum=sum
     }
     sum=0
+    a.push(a[0])
+    a.shift()
 }
 console.log(max_sum)
