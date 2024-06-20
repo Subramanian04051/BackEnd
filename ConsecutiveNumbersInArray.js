5129fbb9dc55749fb1de4d39d98427ff7c03beaa
Subramanian Muthu kumar <subramanian04051@gmail.com>
2024-06-20 12:58:45 +0530

    Update ConsecutiveNumbersInArray.js for removing math functions

--- ConsecutiveNumbersInArray.js
+++ ConsecutiveNumbersInArray.js
@@ -1,13 +1,24 @@
-let a=[8,10,12,11,7],n=a.length-1,cons=true
+let a=[8,10,12,11,7],n=a.length-1,cons='yes'
 //a.sort()
-let max=Math.max(...a)
-let min=Math.min(...a)
+let max=a[0]
+let min=a[0]
+for(let i=0;i<a.length;i++)
+{
+    if(max<a[i])
+    {
+        max=a[i]
+    }
+    if(min>a[i])
+    {
+        min=a[i]
+    }
+}
 console.log(a)
 for(let i=min;i<max;i++)
 {
     if(!a.includes(i))
     {
-        cons=false
+        cons='no'
         break
     }
     
